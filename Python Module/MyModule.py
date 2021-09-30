f08946b3bf8b6bcdc328ce3b3889ad2cfd74f3db
The BIM Coordinator <30426235+DaltonGOO@users.noreply.github.com>
2021-09-30 15:06:22 -0500

    some random script

--- Python Module/MyModule.py
+++ Python Module/MyModule.py
@@ -1,2 +1,31 @@
-def myfunc():
+from collections import OrderedDict
+
+
+def hellowWorld():
     return "hello world!"
+
+
+def distinct(seq):
+    return list(OrderedDict.fromkeys(seq))
+
+
+def boolean_to_string(b):
+    return str(b)
+
+
+def snail(array):
+    OUT = []
+    while array:
+        if array:
+            for i in array.pop(0):
+                OUT.append(i)
+        if array:
+            for i in array:
+                OUT.append(i.pop(-1))
+        if array:
+            for i in array.pop(-1)[::-1]:
+                OUT.append(i)
+        if array:
+            for i in array[::-1]:
+                OUT.append(i.pop(0))
+    return OUT
