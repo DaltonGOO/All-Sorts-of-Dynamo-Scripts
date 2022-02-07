0cf49e8852c60ab957061133faaa2753a7b09bf9
The BIM Coordinator <dalton.c.goodwin@gmail.com>
2022-02-06 21:12:54 -0600

    Create PythonTemplate.py
    I added my python template to this library.

---
+++ lib/PythonTemplate.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("C:\\Users\\dalto\\anaconda3\\envs\\Dynamo_210\\Lib")
+sys.path.append("C:\\Users\\dalto\\OneDrive\\My Stuff\\GitHub\\All-Sorts-of-Dynamo-Scripts\\Python Module")
+
+import clr
+
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import *
+
+clr.AddReference('RevitAPIUI')
+from Autodesk.Revit.UI import *
+
+clr.AddReference('System')
+from System.Collections.Generic import List
+
+clr.AddReference('RevitNodes')
+import Revit
+clr.ImportExtensions(Revit.GeometryConversion)
+clr.ImportExtensions(Revit.Elements)
+
+clr.AddReference('RevitServices')
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc=DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
+#Preparing input from dynamo to revit
+element = UnwrapElement(IN[0])
+
+#Do some action in a Transaction
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+TransactionManager.Instance.TransactionTaskDone()
+
+OUT = element
